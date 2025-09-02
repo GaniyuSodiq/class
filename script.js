ab346dd6500ae87b428e48ef23175daaf5633712
GaniyuSodiq <82044466+GaniyuSodiq@users.noreply.github.com>
2025-09-02 09:00:06 +0100

    use the setter

--- script.js
+++ script.js
@@ -4,9 +4,19 @@ let user = {
 
     get fullname(){
         return `${this.name} ${this.surname}`
+    },
+
+    set fullname(value){
+       [this.name, this.surname] = value.split(" ")
     }
+    
 }
 
 // We don’t call user.fullName as a function, we read it normally: 
 // the getter runs behind the scenes.
-console.log(user.fullname)
+console.log(user.fullname) // Smith Cooper
+
+// set fullName is executed with the given value.
+user.fullname = "Sodiq Ganiyu"
+
+console.log(user.fullname) // Sodiq Ganiyu
